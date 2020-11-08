de0bb9687af40d5f52bc79a80c6a5e1ccd596a8d
MiwelR <rrriosss@gmail.com>
2020-11-09 00:07:05 +0100

    Map, Filter y Reduce

---
+++ mapFilterReduce.py
@@ -0,0 +1,23 @@
+from functools import reduce
+
+def doble(x):
+    return x+x
+
+lista = [1, 3, -1, 15, 9]
+
+
+# Map: Actuar sobre cada uno de los elementos
+listaDobles = map(lambda x: x*2, lista)
+listaDobles1 = map(doble, lista)
+
+def esPar(x):
+    return x % 2 == 0
+
+# Filter: Filtrar los valores de "lista" que cumplan la funcion correspondiente
+listaPares = filter(lambda x: x % 2 == 0, lista)
+listaPares1 = filter(esPar, lista)
+
+# Reduce:
+sumatorio = reduce(lambda x, y: x + y, lista)
+sumatorioDobles = reduce(lambda x, y: x + y * 2, lista)
+suma100 = reduce(lambda x, y: x + y, range(101))
